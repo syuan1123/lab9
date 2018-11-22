7c759302776dcfb02afdeba286f255f4e8775860
syuan1123 <asd20212@gmail.com>
2018-11-22 14:21:50 +0800

    lab9

---
+++ app/src/main/java/com/example/user/myapplication/MyService.java
@@ -0,0 +1,37 @@
+package com.example.user.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+            return null;
+    }
+
+    public  void onCreate() {
+        new  Thread() {
+
+            boolean flag = true;
+            int count = 0;
+
+            public  void run() {
+                while(flag) {
+                    Intent i =new Intent("MyMessage");
+                    i.putExtra("background_service",count);
+                    sendBroadcast(i);
+                    count++;
+                    try{
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+}
